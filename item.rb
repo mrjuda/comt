012b29fced126008951438b9b3f704adddbbbc1f
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-13 21:14:54 +0300

    Implement Item class methods

--- item.rb
+++ item.rb
@@ -15,4 +15,13 @@ class Item
     @genre = genre
     @label = label
   end
+
+  def can_be_archived?
+    return true if ((DateTime.now - @publish_date) / 365).to_i > 10
+    false
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
