f5728903cbd47983b87804893cd0ade7b2196f4b
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-13 21:23:54 +0300

    Add user options

---
+++ main.rb
@@ -0,0 +1,16 @@
+def main
+  puts 'Choose an option: '
+  puts '[1] List all books'
+  puts '[2] List all music albums'
+  puts '[3] List all movies'
+  puts '[4] List of games'
+  puts '[5] List all genres'
+  puts '[7] List all labels'
+  puts '[8] List all authors'
+  puts '[9] List all sources'
+  puts '[10] Add a book'
+  puts '[11] Add a music album'
+  puts '[12] Add a movie'
+  puts '[13] Add a game'
+  puts '[14] Exit'
+end
