6c4211f5215a9c0ce794f1e952b961d8f6044968
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-13 21:12:05 +0300

    Add custom setter

--- item.rb
+++ item.rb
@@ -3,4 +3,16 @@ require 'date'
 class Item
   attr_reader :id, :archived
   attr_accessor :author, :genre, :label, :publish_date
+
+  def initialize(id, publish_date)
+    @id = id || Random.rand(1..1000)
+    @archived = false
+    @publish_date = Date.parse(publish_date)
+  end
+
+  def add_props(author, genre, label)
+    @author = author
+    @genre = genre
+    @label = label
+  end
 end
