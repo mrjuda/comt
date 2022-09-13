4a3a611018147e6f752d6e47eb0441513464bb84
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-13 22:54:46 +0300

    Create constructors

--- genre.rb
+++ genre.rb
@@ -1,2 +1,12 @@
-class Genre
+require_relative 'item'
+
+class Genre < Item
+  attr_accessor :name, :items
+  attr_reader :id
+
+  def initialize(id, name)
+    @id = id || Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
 end
--- music_album.rb
+++ music_album.rb
@@ -1,2 +1,7 @@
 class MusicAlbum
+  attr_accessor :title, :on_spotify
+
+  def initialize(on_spotify)
+    @on_spotify = on_spotify
+  end
 end
