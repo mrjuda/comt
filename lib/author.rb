96c8cfda0c4e09f736dec98c4eb8101d0188189e
Bushido-brown <orizut@gmail.com>
2022-09-14 06:15:46 -0700

    FIX linter errors

--- lib/author.rb
+++ lib/author.rb
@@ -2,13 +2,13 @@ class Author
   attr_accessor :first_name, :last_name
 
   def initialize(first_name = 'Undefined', last_name = 'Undefined')
-   @last_name = last_name
-   @first_name = first_name
-   @items = []
+    @last_name = last_name
+    @first_name = first_name
+    @items = []
   end
 
   def add_item(item)
-   @items.push(item) unless @items.include?(item)
-   item.author = self
+    @items.push(item) unless @items.include?(item)
+    item.author = self
   end
 end
