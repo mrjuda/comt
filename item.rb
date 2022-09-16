31b562d18c840ad11b3cf87fbcfa2b5bf73fe4b4
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-17 01:18:39 +0300

    Create music data functions

--- item.rb
+++ item.rb
@@ -3,12 +3,11 @@ class Item
   attr_accessor :publish_date, :author, :label, :genre
   attr_reader :archived
 
-  def initialize(publish_date, archived: false)
+  def initialize(publish_date, genre, author, archived: false)
     @publish_date = publish_date
     @archived = archived
-    @author = nil
-    @label = nil
-    @genre = nil
+    @genre = genre
+    @author = author
   end
 
   def move_to_archive
@@ -22,12 +21,7 @@ class Item
 
   def add_author(author)
     @author = author
-    author.add_item(self)
-  end
-
-  def add_label(label)
-    @label = label
-    label.add_item(self)
+    genre.add_item(self)
   end
 
   private
